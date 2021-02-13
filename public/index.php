9ff3f19db1957a16ba345e84c6a85c485b5e069a
inidwiii <dwigamers30@gmail.com>
2021-02-13 15:06:11 +0800

    Creating main file
    Creating index.php file as main file for booting up the application

---
+++ public/index.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Auryn - A Simple PHP Framework
+ * 
+ * This framework is a simple PHP based framework to help other
+ * developer to developing their application. 
+ * 
+ * @author inidwiii <dwigamers30@gmail.com>
+ * @package Auryn
+ */
+
+require realpath(__DIR__ . '/../vendor/autoload.php');
+
+/**
+ * Initializing the application
+ */
+
+$app = new \Auryn\Core\Application();
