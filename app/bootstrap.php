6f4b862c233d112047959a1c2e7fa236cd6e599c
inidwiii <dwigamers30@gmail.com>
2021-02-15 01:29:44 +0800

    Adding dependencies preloader
    Adding feature where the app can load the dependencies before the app is initialized

---
+++ app/bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+
+use Auryn\Core\Env;
+
+return [
+  /**
+   * Hold the pre-registered instance
+   */
+
+  /**
+   * Hold the pre-registered instance as a singleton instance
+   */
+  // 'singletons' => [
+  //   'env' => Env::class
+  // ]
+];
--- src/Auryn/Core/Application.php
+++ src/Auryn/Core/Application.php
@@ -7,15 +7,28 @@ class Application extends Container
   public function __construct()
   {
     $this->preloadConstants();
+    $this->preloadDependencies();
   }
 
+  private function preloadDependencies()
+  {
+    foreach (require realpath(PATH_APP . 'bootstrap.php') as $key => $value) {
+      if ($key === 'singletons') foreach ($value as $abstract => $concrete) parent::singleton($abstract, $concrete);
+      else parent::bind($value, $value);
+    }
+  }
+
+  /**
+   * Pre-defined constant
+   * 
+   * @return void
+   */
   private function preloadConstants()
   {
     defined('DS') or define('DS', DIRECTORY_SEPARATOR);
-    defined('ROOT') or define('ROOT', realpath(getcwd()) . DS);
-
-    defined('PATH_TO_LIB') or define('PATH_TO_LIB', realpath(ROOT . 'src/Auryn') . DS);
-    defined('PATH_TO_CONFIG') or define('PATH_TO_CONFIG', realpath(ROOT . 'config') . DS);
-    defined('PATH_TO_CORE') or define('PATH_TO_CORE', realpath(PATH_TO_LIB . 'core') . DS);
+    defined('ROOT') or define('ROOT', realpath(__DIR__ . '/../../..') . DS);
+    defined('PATH_APP') or define('PATH_APP', realpath(ROOT . 'app') . DS);
+    defined('PATH_LIB') or define('PATH_LIB', realpath(ROOT . 'src/Auryn') . DS);
+    defined('PATH_CORE') or define('PATH_CORE', realpath(PATH_LIB . 'Core') . DS);
   }
 }
